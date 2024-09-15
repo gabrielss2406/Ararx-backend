db3526d9041de9c7224dbc7724f3c97a8a2bd5c0
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2024-09-15 00:36:28 -0300

    feat: improving function read_test

--- api/api.py
+++ api/api.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 from api.services.db import connect_mongo
@@ -12,11 +14,14 @@ def read_root():
 
 # Testando a conexão com o banco de dados. Deve ser removido posteriormente
 @app.get("/test")
-def read_test():
+def read_test() -> Union[list[dict], None]:
     collection, client = connect_mongo('Users')
     result = collection.find({})
 
-    for document in result:
-        print(document)
+    documents: list[dict] = []
 
-    return None
+    for document in result:
+        document["_id"] = str(document["_id"])
+        documents.append(document)
+
+    return documents
