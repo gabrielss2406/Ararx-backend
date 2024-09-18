2c3ea469cb1cca22719071a31e5982c437303115
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2024-09-18 20:20:33 -0300

    feat: add followers and following to UserOut

--- api/models/UsersModels.py
+++ api/models/UsersModels.py
@@ -3,18 +3,21 @@ from typing import Optional
 
 
 class UserIn(BaseModel):
-    username: str = Field(max_length=50)
+    handler: str = Field(max_length=50)
     password: str = Field(max_length=50)
     email: EmailStr = Field(max_length=50)
 
 
 class UserOut(UserIn):
+    username: str = Field(max_length=50)
     bio: str = Field(max_length=240)
+    followers: list[str]
+    follwing: list[str]
 
 
 class UserUpdateQuery(BaseModel):
     email: Optional[EmailStr] = Field(max_length=50)
     password: Optional[str] = Field(max_length=50)
-    nickname: Optional[str] = Field(max_length=50)
+    handler: Optional[str] = Field(max_length=50)
     bio: Optional[str] = Field(max_length=240)
     username: Optional[str] = Field(max_length=50)
